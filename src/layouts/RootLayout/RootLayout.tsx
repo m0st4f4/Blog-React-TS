f90cbca561b9a142a38bb1fc5cee3ac0b0a0e454
mostafa hosseini <smostafah89@gmail.com>
2026-06-14 17:05:45 +0330

    feat: add HeaderComponent

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,16 +1,12 @@
 import type {ReactNode} from "react";
 import {Header} from "@/components/Header/Header.tsx";
-import {Footer} from "@/components/Footer/Footer.tsx";
 import {Outlet} from "react-router";
 
 export const RootLayout = (): ReactNode => {
     return <div className="">
-
         <Header/>
         <main>
             <Outlet/>
-
         </main>
-        <Footer/>
-    </div>;
+    </div>
 };
