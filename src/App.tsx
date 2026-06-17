e69a324bfbdd60665e75ec7b42e37f85740d0800
Mostafa Hosseini <smostafah89@gmail.com>
2026-06-17 20:57:25 +0330

    feat: add article page and route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import {Route, Routes} from "react-router";
 import {RootLayout} from "@/layouts/RootLayout/RootLayout.tsx";
 import {HomePage} from "@/pages/HomePage/HomePage.tsx";
+import {ArticlePage} from "@/pages/ArticlePage/ArticlePage.tsx";
 
 function App() {
-
     return (
         <Routes>
             <Route element={<RootLayout/>}>
                 <Route index element={<HomePage/>}/>
+                <Route path="article/:id" element={<ArticlePage/>}/>
             </Route>
         </Routes>
     );
---
+++ src/pages/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,7 @@
+import type {ReactNode} from "react";
+
+export const ArticlePage = (): ReactNode => {
+    return (
+        <div>Article Page</div>
+    );
+};
