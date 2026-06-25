d740f262730b44d86071439ac51d25602482f5ad
Mostafa Hosseini <smostafah89@gmail.com>
2026-06-25 20:29:18 +0330

    feat: formatDate using i18n data
    add useLocalizedDate hook and formatDate using i18n data

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,33 +1,37 @@
-import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
 import locales from "@/locales";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
 export const languages: Record<string, { nativeName: string }> = {
-    en: {nativeName: 'English'},
-    fa: {nativeName: 'فارسی'},
-    ar: {nativeName: 'العربية'}
+  en: { nativeName: "English" },
+  fa: { nativeName: "فارسی" },
+  ar: { nativeName: "العربية" },
 };
 
 const resources = {
-    en: {
-        translation: locales.en
-    },
-    fa: {
-        translation: locales.fa
-    },
-    ar: {
-        translation: locales.ar
-    }
+  en: {
+    translation: locales.en,
+    locale: "en-US",
+    calendar: "gregory",
+  },
+  fa: {
+    translation: locales.fa,
+    locale: "fa-IR",
+    calendar: "persian",
+  },
+  ar: {
+    translation: locales.ar,
+    locale: "ar-SA",
+    calendar: "islamic",
+  },
 };
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources,
-        lng: "en",
-        interpolation: {
-            escapeValue: false
-        }
-    });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "fa",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
