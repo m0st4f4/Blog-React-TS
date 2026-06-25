f54d6c0e500451d6056fd4aa628ce74a7ee373b1
Mostafa Hosseini <smostafah89@gmail.com>
2026-06-26 01:13:19 +0330

    feat: add Error Boundary
    install react-error-boundary , add ErrorPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 
 import { BrowserRouter } from "react-router";
 
+import { ErrorBoundary } from "react-error-boundary";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { DirectionProvider } from "@/components/ui/direction.tsx";
+
+import { ErrorPage } from "@/pages/ErrorPage/ErrorPage.tsx";
 
 import App from "./App.tsx";
 import "./i18n";
@@ -42,13 +46,15 @@ window.__TANSTACK_QUERY_CLIENT__ = queryClient;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <DirectionProvider dir="ltr">
-        <QueryClientProvider client={queryClient}>
-          <App />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </DirectionProvider>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <BrowserRouter>
+        <DirectionProvider dir="ltr">
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </DirectionProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 );
