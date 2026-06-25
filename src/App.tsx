f9abf687bed5bdcb25385fc52a0a3b240ab47c24
Mostafa Hosseini <smostafah89@gmail.com>
2026-06-26 02:40:58 +0330

    feat: add category page (#40)
    add CategoryPage and category route
    
    #22

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import { RootLayout } from "@/layouts/RootLayout/RootLayout.tsx";
 
 import { ArticlePage } from "@/pages/ArticlePage/ArticlePage.tsx";
+import { CategoryPage } from "@/pages/CategoryPage/CategoryPage.tsx";
 import { HomePage } from "@/pages/HomePage/HomePage.tsx";
 import { NotFoundPage } from "@/pages/NotFoundPage/NotFoundPage.tsx";
 
@@ -14,6 +15,7 @@ function App() {
       <Route element={<RootLayout />}>
         <Route index element={<HomePage />} />
         <Route path="article/:id" element={<ArticlePage />} />
+        <Route path="category/:id" element={<CategoryPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
