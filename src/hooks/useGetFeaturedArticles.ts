5c5becb699574912117c9d6e3b946b19150e6d7d
m0st4f4 <smostafah89@gmail.com>
2026-07-08 10:21:50 +0330

    refactor(hook): send params for filter featured articles

--- src/hooks/useGetFeaturedArticles.ts
+++ src/hooks/useGetFeaturedArticles.ts
@@ -1,11 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { fetchFeaturedArticles } from "@/services/articleService.ts";
+import { fetchFilteredArticles } from "@/services/articleService.ts";
 
 export const useGetFeaturedArticles = () => {
   const { data, isPending, isError } = useQuery({
     queryKey: ["articles", "featured"],
-    queryFn: () => fetchFeaturedArticles(),
+    queryFn: () =>
+      fetchFilteredArticles({
+        isFeatured: true,
+        _sort: "id",
+        _order: "desc",
+        status: "published",
+        _expand: ["category"],
+        _page: 1,
+        _limit: 5,
+      }),
   });
 
   return { data, isPending, isError };
