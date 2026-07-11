bcad0a335f20ab97fd7d701c45ff14f77b6943e6
Mostafa Hosseini <smostafah89@gmail.com>
2026-07-12 03:09:54 +0330

    feat(ui): add aboutUs and contactUs pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Route, Routes } from "react-router";
 import { RootLayout } from "@/layouts/RootLayout/RootLayout.tsx";
 import { SidebarLayout } from "@/layouts/SidebarLayout/SidebarLayout.tsx";
 
+import { AboutPage } from "@/pages/AboutPage/AboutPage.tsx";
 import { ArticlePage } from "@/pages/ArticlePage/ArticlePage.tsx";
 import { CategoryPage } from "@/pages/CategoryPage/CategoryPage.tsx";
+import { ContactPage } from "@/pages/ContactPage/ContactPage.tsx";
 import { HomePage } from "@/pages/HomePage/HomePage.tsx";
 import { NotFoundPage } from "@/pages/NotFoundPage/NotFoundPage.tsx";
 import { SearchPage } from "@/pages/SearchPage/SearchPage.tsx";
@@ -14,6 +16,8 @@ function App() {
     <Routes>
       <Route element={<RootLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="aboutus" element={<AboutPage />} />
+        <Route path="contactus" element={<ContactPage />} />
         <Route path="article" element={<SidebarLayout />}>
           <Route path=":id" element={<ArticlePage />} />
         </Route>
---
+++ src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,10 @@
+import { type ReactNode } from "react";
+
+export const ContactPage = (): ReactNode => {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+    </div>
+  );
+};
+
