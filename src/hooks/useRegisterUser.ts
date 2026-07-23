625d6031a1c4acf7d6353c22e6ddd1f225ffb89a
Mostafa Hosseini <smostafah89@gmail.com>
2026-07-23 17:55:39 +0330

    style: format userRegisterUser hook

--- src/hooks/useRegisterUser.ts
+++ src/hooks/useRegisterUser.ts
@@ -3,11 +3,12 @@ import { useMutation } from "@tanstack/react-query";
 import { RegisterUser, type RegisterUserType } from "@/services/userService.ts";
 
 export const useRegisterUser = () => {
-  const { mutate, isPending, isSuccess, data, isError, error,reset } = useMutation({
-    mutationFn: (data: RegisterUserType) => {
-      return RegisterUser(data);
-    },
-  });
+  const { mutate, isPending, isSuccess, data, isError, error, reset } =
+    useMutation({
+      mutationFn: (data: RegisterUserType) => {
+        return RegisterUser(data);
+      },
+    });
 
-  return { mutate, isPending, isSuccess, data, isError, error ,reset};
+  return { mutate, isPending, isSuccess, data, isError, error, reset };
 };
