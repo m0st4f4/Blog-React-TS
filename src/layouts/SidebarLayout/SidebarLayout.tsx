b21c8a794ec18262ccc8dffd4ee43b770c201132
Mostafa Hosseini <smostafah89@gmail.com>
2026-07-27 17:51:03 +0330

    fix: move container class from pages to rootLayout

--- src/layouts/SidebarLayout/SidebarLayout.tsx
+++ src/layouts/SidebarLayout/SidebarLayout.tsx
@@ -6,7 +6,7 @@ import { Sidebar } from "@/components/Sidebar/Sidebar.tsx";
 
 export const SidebarLayout = (): ReactNode => {
   return (
-    <div className="container grid gap-4 pt-12 md:grid-cols-4">
+    <div className="grid gap-4 pt-12 md:grid-cols-4">
       <div className="col-span-full md:col-span-3">
         <Outlet />
       </div>
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from "react";
 
 export const AboutPage = (): ReactNode => {
   return (
-    <div className="container">
+    <div>
       <h1>About Us</h1>
     </div>
   );
